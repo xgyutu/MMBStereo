8b192890b5d77811dd588d89cb60070b9d80b110
xgyutu <119731546+xgyutu@users.noreply.github.com>
2025-09-28 14:24:54 +0800

    Update save_disp.py

--- save_disp.py
+++ save_disp.py
@@ -1,56 +1,41 @@
-from __future__ import print_function, division
 import argparse
 import os
 import torch
 import torch.nn as nn
-import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data
-from torch.autograd import Variable
-import torchvision.utils as vutils
-import torch.nn.functional as F
 import numpy as np
 import time
-from tensorboardX import SummaryWriter
 from datasets import __datasets__
 from models import __models__
 from utils import *
 from torch.utils.data import DataLoader
-import gc
 import skimage
 import skimage.io
 import cv2
-# cudnn.benchmark = True
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-parser = argparse.ArgumentParser(description='Accurate and Efficient Stereo Matching via Attention Concatenation Volume (Fast-ACV)')
-parser.add_argument('--model', default='TAM-Stereo', help='select a model structure', choices=__models__.keys())
+parser = argparse.ArgumentParser(description='MMB-Stereo: Mamba-based Multi-branch Cost Aggregation - Disparity Saving')
+parser.add_argument('--model', default='MMBStereo', help='select a model structure', choices=__models__.keys())
 parser.add_argument('--maxdisp', type=int, default=192, help='maximum disparity')
 parser.add_argument('--dataset', default='kitti', help='dataset name', choices=__datasets__.keys())
-parser.add_argument('--kitti15_datapath', default='/root/autodl-tmp/KITTI/KITTI_2015/', help='data path')
-parser.add_argument('--kitti12_datapath', default='/root/autodl-tmp/KITTI/KITTI_2012/', help='data path')
+parser.add_argument('--kitti15_datapath', default='/root/autodl-tmp/KITTI/KITTI_2015/', help='KITTI 2015 data path')
+parser.add_argument('--kitti12_datapath', default='/root/autodl-tmp/KITTI/KITTI_2012/', help='KITTI 2012 data path')
 parser.add_argument('--testlist', default='./filenames/kitti15_test.txt', help='testing list')
-parser.add_argument('--loadckpt', default='./runs/kitti/TAM-Stereo/checkpoint_000499.ckpt',help='load the weights from a specific checkpoint')
-# parse arguments
+parser.add_argument('--loadckpt', default='./runs/kitti/MMB-Stereo/checkpoint_000499.ckpt', help='load weights from specific checkpoint')
 args = parser.parse_args()
 
-# dataset, dataloader
 StereoDataset = __datasets__[args.dataset]
 test_dataset = StereoDataset(args.kitti15_datapath, args.kitti12_datapath, args.testlist, False)
 TestImgLoader = DataLoader(test_dataset, 1, shuffle=False, num_workers=4, drop_last=False)
 
-# model, optimizer
 model = __models__[args.model](args.maxdisp)
 model = nn.DataParallel(model)
 model.cuda()
 
-#load parameters
 print("loading model {}".format(args.loadckpt))
 state_dict = torch.load(args.loadckpt)
 model.load_state_dict(state_dict['model'])
-
 
 save_dir = './output/kitti15/disp_0'
 
@@ -73,10 +58,8 @@ def test():
             print("saving to", fn, disp_est.shape)
             disp_est_uint = np.round(disp_est * 256).astype(np.uint16)
             skimage.io.imsave(fn, disp_est_uint)
-            # cv2.imwrite(fn, cv2.applyColorMap(cv2.convertScaleAbs(disp_est_uint, alpha=0.01),cv2.COLORMAP_JET))
 
 
-# test one sample
 @make_nograd_func
 def test_sample(sample):
     model.eval()
